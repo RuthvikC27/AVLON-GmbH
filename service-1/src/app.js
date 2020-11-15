7369f5e23e591b12bd911dc7d8bb835289c82a8f
ruthvikc27 <ruthvikvanam27@gmail.com>
2020-11-15 10:30:12 +0530

    Initial

--- service-1/src/app.js
+++ service-1/src/app.js
@@ -1,8 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from 'mongoose';
 
 const app = express();
 
+// LISTENING
 const PORT = 5000;
 app.listen(PORT, () => {
+    mongoose.connect("mongodb://service-1-mongo-srv:27017/service-1", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => {
+        console.log("DB connected!");
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
     console.log(`service-1 started on http://localhost:${PORT}`)
 })
